164951fab7d49b6f3b69ac32f6c249360c542c77
Orivaldo Eder Matavelli <eder.matavelli@gmail.com>
2026-03-03 22:02:54 -0300

    Add files via upload

---
+++ ExercicioMultiplicacao.py
@@ -0,0 +1,26 @@
+# Exercício de Programação Python
+# Março de 2026
+# Prof. Filipe
+
+print ("##########################################################")
+
+print ("                    MULTIPLICAÇÃO PYTHON                  ")
+
+print ("##########################################################") 
+
+
+a = input("Digite o 1o. número:   ")
+a = int(a)
+
+b = input("Digite o 2o. número:   ")
+b = int(b)
+
+c= a * b
+
+print ("##########################################################")
+print ("           Agora vamos MULTIPLICAR a x b                  ")
+print ("##########################################################")
+
+print("A resposta é:  ",c)
+
+input ()
---
+++ ExercicioRadiciacao.py
@@ -0,0 +1,26 @@
+# Exercício de Programação Python
+# Março de 2026
+# Prof. Filipe
+
+print ("##########################################################")
+
+print ("                    RADICIAÇÃO PYTHON                     ")
+
+print ("##########################################################") 
+
+
+a = input("Digite o 1o. número:   ")
+a = int(a)
+
+b = input('Digite um número "inteiro" da Raiz:    ')
+b = int(b)
+
+c = a ** (1/b)
+
+print ("##########################################################")
+print ('           Agora vamos extrair a Raiz de "a"              ')
+print ("##########################################################")
+
+print ("A resposta é:  ",c)
+
+input ()
